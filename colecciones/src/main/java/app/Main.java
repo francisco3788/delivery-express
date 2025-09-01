cfc7a7dfbf7b692cd2ffb49a51e54e47b78e30ad
yulbastidas <yuly.bastidas@campusucc.edu.co>
2025-08-31 22:02:52 -0500

    comit main y pedido

---
+++ colecciones/src/main/java/app/Main.java
@@ -0,0 +1,12 @@
+package main.java.app;
+
+import main.java.colecciones.Ejercicios;
+
+public class Main {
+    public static void main(String[] args) {
+        Ejercicios.parteA();
+        Ejercicios.parteB();
+        Ejercicios.parteC();
+        Ejercicios.parteD();
+    }
+}
